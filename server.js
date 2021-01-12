25da0cfedb10c0a4119c5024863db7e26b85c145
Ale Rodríguez <est.aedrz@gmail.com>
2021-01-12 12:07:47 -0500

    dotenv for environment variables and other syntax adjustments

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-// server.js
-// where your node app starts
+const express = require('express');
+const app = express();
 
-// init project
-let express = require('express');
-let app = express();
+require('dotenv').config({ path: './.env' })
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-let cors = require('cors');
+const cors = require('cors');
 app.use(cors({
   optionsSuccessStatus: 200
-})); // some legacy browsers choke on 204
+}));
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -53,16 +47,8 @@ app.get('/api/timestamp/:date', (req, res) => {
   }
 });
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({
-    greeting: 'hello API'
-  });
-});
-
 const port = process.env.PORT || 3000;
 
-// listen for requests :)
 let listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
